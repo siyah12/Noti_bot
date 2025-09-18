1b63e6397516524105e51fc26afbeca9d5563472
siyah12 <unknownperson.bd.2.0@gmail.com>
2025-09-18 11:52:41 +0600

    Update supportgc.js

--- scripts/cmds/supportgc.js
+++ scripts/cmds/supportgc.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ api, event, threadsData, getLang, message }) {
-    const supportGroupThreadID = "27455554110724563"; // Replace with your support group thread ID
+    const supportGroupThreadID = "1428085604968546"; /https://m.me/j/AbaCPrsEeMYvAXUk
     const botID = api.getCurrentUserID();
 
     try {
